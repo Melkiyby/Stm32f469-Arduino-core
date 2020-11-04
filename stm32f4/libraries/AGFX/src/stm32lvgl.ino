d5d1e7b91b2bcdec3cd9b5be1bc89e1ecd6a3d3b
Melkiyby <melkiy.by@gmail.com>
2020-11-04 17:40:41 +0300

    Update stm32lvgl.ino

--- stm32f4/libraries/AGFX/src/stm32lvgl.ino
+++ stm32f4/libraries/AGFX/src/stm32lvgl.ino
@@ -1,17 +1,33 @@
-//#include "lvgl.h"
+#include <lvgl.h>
 #include <tft.h>
-//#include "hal_stm_lvgl/touchpad/touchpad.h"
-//#include "C:/Sketches/libraries/lvgl/examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
+#include <touchpad.h>
+
+void my_printf(lv_log_level_t level, const char *file, uint32_t line, const char *dsc, const char *dsc1)
+{
+    Serial3.printf("level:%d,file:%s,line:%d,dsc:%s,%s\r\n",level,file,line,dsc, dsc1);
+    Serial3.flush();
+}
+
 void setup() {
-  // put your setup code here, to run once:
-//  lv_init();
-  tft_init();
-//  touchpad_init();
-//  terminal_create();
-///  lv_demo_benchmark();
+  
+Serial3.begin(115200);
+ lv_init();
+ tft_init();
+ touchpad_init();
+ lv_log_register_print_cb(my_printf);
+
+ lv_obj_t *label = lv_label_create(lv_scr_act(), NULL);
+ lv_label_set_text(label, "Hello Arduino! (V7.0.X)");
+ lv_obj_align(label, NULL, LV_ALIGN_CENTER, 100, 100);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedl:
+    lv_tick_inc(5);
+    lv_task_handler();
+    
+    BSP_LED_Toggle(LED3);
+    delay(5);
 
 }
